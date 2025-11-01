d215882d81b5c8e6f9c87488c7a97d5b48942a08
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-01 11:12:18 +0100

    feat: implement methods into userApi

--- src/api/users/userApi.ts
+++ src/api/users/userApi.ts
@@ -3,5 +3,6 @@ import axiosClient from "../axiosClient.ts";
 export const userApi = {
     getAllUsers: () => axiosClient.get("/user"),
     getUserSearch: (params: {id?:string; name?: string; email?: string}) => axiosClient.get("/user/search", {params}),
-
+    updateUser: (id: string,data) => axiosClient.post(`/user/update/${id}`, data),
+    deleteUser: (id: string) => axiosClient.delete(`/user/${id}`),
 }
