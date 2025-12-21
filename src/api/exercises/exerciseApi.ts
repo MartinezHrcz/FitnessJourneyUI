88f66316ad8ad585d642630173b26ddb7b579452
Herczeg Martinez <herczegmartinez@gmail.com>
2025-12-21 20:10:33 +0100

    feat: add exercise api

---
+++ src/api/exercises/exerciseApi.ts
@@ -0,0 +1,32 @@
+import type {AbstractExerciseDTO} from "../../types/fitness/Exercise.ts";
+import axiosClient from "../axiosClient.ts";
+import type {AbstractSetDTO} from "../../types/fitness/Set.ts";
+
+export const exerciseApi = {
+    create: (data: AbstractExerciseDTO) =>
+        axiosClient.post<AbstractExerciseDTO>("/exercise", data),
+
+    getAll: () =>
+        axiosClient.get<Array<AbstractExerciseDTO>>("/exercise"),
+
+    getById: (id: string) =>
+        axiosClient.get<AbstractExerciseDTO>(`/exercise/${id}`),
+
+    getByName: (name: string) =>
+        axiosClient.get<Array<AbstractExerciseDTO>>(`/exercise/byname/${name}`),
+
+    getByUser: (userId: string) =>
+        axiosClient.get<Array<AbstractExerciseDTO>>(`/exercise/byuser/${userId}`),
+
+    getByWorkout: (workoutId: string) =>
+        axiosClient.get<Array<AbstractExerciseDTO>>(`/exercise/byworkout/${workoutId}`),
+
+    delete: (id: string) =>
+        axiosClient.delete(`/exercise/${id}`),
+
+    addSet: (exerciseId: string, setData: AbstractSetDTO) =>
+        axiosClient.put<AbstractExerciseDTO>(`/exercise/addset/${exerciseId}`, setData),
+
+    removeSet: (exerciseId: string, setId: number) =>
+        axiosClient.delete<AbstractExerciseDTO>(`/exercise/removeset/${exerciseId}-${setId}`)
+}
--- src/api/workouts/workoutApi.ts
+++ src/api/workouts/workoutApi.ts
@@ -2,18 +2,33 @@ import axiosClient from "../axiosClient.ts";
 import type {WorkoutCreateDTO, WorkoutDTO, WorkoutUpdateDTO} from "../../types/fitness/Workout.ts";
 
 export const workoutApi = {
-    getAll: () => axiosClient.get<Array<WorkoutDTO>>("/workout"),
-    getById: (id: string) => axiosClient.get<WorkoutDTO>(`/workout/${id}`),
-    getByUserId: (userId: string) => axiosClient.get<WorkoutDTO>(`/workout/byuser/${userId}`),
-    create: (data: WorkoutCreateDTO) => axiosClient.post<string>(`/workout`, data),
-    update: (id: string, data: WorkoutUpdateDTO) => axiosClient.put<WorkoutDTO>(`/workout/${id}`, data),
-    delete: (id: string) => axiosClient.delete(`/workout/${id}`),
+    getAll: () =>
+        axiosClient.get<Array<WorkoutDTO>>("/workout"),
+
+    getById: (id: string) =>
+        axiosClient.get<WorkoutDTO>(`/workout/${id}`),
+
+    getByUserId: (userId: string) =>
+        axiosClient.get<WorkoutDTO>(`/workout/byuser/${userId}`),
+
+    create: (data: WorkoutCreateDTO) =>
+        axiosClient.post<string>(`/workout`, data),
+
+    update: (id: string, data: WorkoutUpdateDTO) =>
+        axiosClient.put<WorkoutDTO>(`/workout/${id}`, data),
+
+    delete: (id: string) =>
+        axiosClient.delete(`/workout/${id}`),
+
     addExerciseToWorkout: (workoutId: string, exerciseId: string) =>
         axiosClient.put<WorkoutDTO>(`/workout/addexc/${workoutId}-${exerciseId}`),
+
     addDefaultExerciseToWorkout: (workoutId: string, templateId: string) =>
         axiosClient.put<WorkoutDTO>(`/workout/addexc/template/default/${workoutId}-${templateId}`),
+
     addUserExerciseToWorkout: (workoutId: string, templateId: string) =>
         axiosClient.put<WorkoutDTO>(`/workout/addexc/template/usermade/${workoutId}-${templateId}`),
+
     removeExerciseFromWorkout: (workoutId: string, exerciseId: string) =>
         axiosClient.delete<WorkoutDTO>(`/workout/rmexc/${workoutId}-${exerciseId}`),
 }
