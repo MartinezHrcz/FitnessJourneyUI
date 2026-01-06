d748daab6b2d6cca2e9d5fc04f833accfbb0172c
Herczeg Martinez <herczegmartinez@gmail.com>
2026-01-06 22:33:38 +0100

    feat: working chat

--- src/components/ChatModal.tsx
+++ src/components/ChatModal.tsx
@@ -1,6 +1,6 @@
 import {Send, X} from "lucide-react";
-import {useEffect, useState} from "react";
-import type {MessageDto} from "../types/social/Message.ts";
+import {useEffect, useRef, useState} from "react";
+import type {CreateMessageDto, MessageDto} from "../types/social/Message.ts";
 import {messageApi} from "../api/messages/messageApi.ts";
 import type {FriendDTO} from "../types/social/Friend.ts";
 
@@ -9,36 +9,40 @@ const ChatModal = ({friend, onClose}: {friend: FriendDTO, userId: string, onClos
     const [messages, setMessages] = useState<MessageDto[]>([]);
     const [newMessage, setNewMessage] = useState("");
     const [loading, setLoading] = useState(true);
-    const [userId, setUserId] = useState("");
-    const [friendId, setFriendId] = useState("");
+    const [currentUserId, setCurrentUserId] = useState("");
+    const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
-        if (friend)
-        {
-            if (friend.isRequester)
-            {
-                setUserId(friend.userId);
-                setFriendId(friend.friendId);
-            }
-            else {
-                setUserId(friend.friendId);
-                setFriendId(friend.userId);
-            }
-            messageApi.getMessages(friend.userId, friend.friendId)
-                .then((res) => {
-                    setMessages(res.data);
-                    setLoading(false);
-                })
-                .catch(err => console.log(err));
+        const storedUser = localStorage.getItem("user");
+        if (storedUser) {
+            const parsedUser = JSON.parse(storedUser);
+            setCurrentUserId(parsedUser.id);
         }
     }, []);
+
+    useEffect(() => {
+        messageApi.getMessages(friend.userId, friend.friendId)
+            .then((res) => {
+                setMessages(res.data);
+                setLoading(false);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
 
     const handleSendMessage = async () => {
         if (!newMessage.trim()) return;
 
-        const payload: MessageDto = {
-            senderId: userId,
-            recipientId: friendId,
+        const payload: CreateMessageDto = {
+            senderId: currentUserId,
+            recipientId: currentUserId == friend.friendId ? friend.userId: friend.friendId,
             content: newMessage
         };
 
@@ -57,12 +61,12 @@ const ChatModal = ({friend, onClose}: {friend: FriendDTO, userId: string, onClos
             <div className="bg-white w-full max-w-md h-[600px] rounded-3xl shadow-2xl flex flex-col overflow-hidden">
                 <div className="bg-blue-600 p-4 text-white flex justify-between items-center">
                     <div className="flex items-center gap-3">
-                        <div className="w-10 h-10 bg-white/20 rounded-full flex items-center justify-center font-bold">
-                            {friend.friendName.charAt(0)}
-                        </div>
-                        <div>
-                            <p className="font-bold">{friend.friendName}</p>
-                        </div>
+                    <div className="w-10 h-10 bg-white/20 rounded-full flex items-center justify-center font-bold">
+                        {friend.friendName.charAt(0)}
+                    </div>
+                    <div>
+                        <p className="font-bold">{friend.friendName}</p>
+                    </div>
                     </div>
                     <button onClick={onClose} className="p-2 hover:bg-white/10 rounded-full transition-colors">
                         <X size={20} />
@@ -71,9 +75,9 @@ const ChatModal = ({friend, onClose}: {friend: FriendDTO, userId: string, onClos
 
                 <div className="flex-1 overflow-y-auto p-4 space-y-4 bg-slate-50">
                     {messages.map((msg) => (
-                        <div key={msg.id} className={`flex flex-col ${msg.senderId === userId ? 'items-end' : 'items-start'}`}>
+                        <div key={msg.id} className={`flex flex-col ${msg.senderId === currentUserId ? 'items-end' : 'items-start'}`}>
                             <div className={`p-3 rounded-2xl max-w-[80%] text-sm shadow-sm ${
-                                msg.senderId === userId
+                                msg.senderId === currentUserId
                                     ? 'bg-blue-600 text-white rounded-tr-none'
                                     : 'bg-white border border-slate-200 text-slate-800 rounded-tl-none'
                             }`}>
@@ -81,11 +85,13 @@ const ChatModal = ({friend, onClose}: {friend: FriendDTO, userId: string, onClos
                             </div>
                         </div>
                     ))}
+                    <div ref={messagesEndRef} />
                 </div>
 
                 <div className="p-4 bg-white border-t border-slate-100 flex gap-2 items-center">
                     <input
                         onChange={(e) => setNewMessage(e.target.value)}
+                        value={newMessage}
                         className="flex-1 bg-slate-100 rounded-2xl px-4 py-3 text-sm outline-none focus:ring-2 focus:ring-blue-500 transition-all"
                     />
                     <button
