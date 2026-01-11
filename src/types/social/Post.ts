e4aaabb4d55820e8abcf1e8d6b8ba4045e8119d7
Herczeg Martinez <herczegmartinez@gmail.com>
2026-01-11 19:17:18 +0100

    chore: create Post types

---
+++ src/types/social/Post.ts
@@ -0,0 +1,19 @@
+export interface PostDto {
+    id: string;
+    title: string;
+    content: string;
+    userId: string;
+    sentTime: Date;
+}
+
+export interface PostCreateDto {
+    title: string;
+    content: string;
+    userId: string;
+}
+
+export interface PostUpdateDto {
+    title: string;
+    content: string;
+    userId: string;
+}
